8c2f27a1b16071ad0dc47651164265193d089b67
ryan <ryan@awkvi.com>
2023-04-25 18:34:33 -0700

    made example map

---
+++ src/components/Layout.js
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar";
+import React from "react";
+
+
+const Layout = () => {
+    return(
+        <>
+        <Navbar />
+        </>
+        
+    );
+
+}
+
+export default Layout;
